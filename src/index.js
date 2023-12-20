6b163b89a1579956dc9e910e8b44c83516ecf6a0
Minotour <quoclan9@gmail.com>
2023-12-21 02:10:58 +0700

    Implementing online uuid (and a shit ton of bugs)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { Client, GatewayIntentBits } from "discord.js";
 import "dotenv/config"
 import uuidOffline from "./uuid.js";
-import { XMLHttpRequest } from "xmlhttprequest";
+import axios from "axios";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -15,26 +15,35 @@ client.on('interactionCreate', async interaction => {
     if (interaction.commandName == "link") {
         const username = interaction.options.get('username').value;
         const type = interaction.options.get('type').value;
+        var user_info = {
+            name: username,
+            id: ''
+        };
         try {
-            let user_info = {
-                name: username
-            };
-            if (type == 'offline') {
+            if (type === 'offline') {
                 user_info.id = uuidOffline(username);
-            } else if (type == 'online') {
-                const req = new XMLHttpRequest();
-                req.addEventListener("loadend", () => {
-                    user_info = this.responseText;
-                })
-                req.open("GET", `https://api.mojang.com/users/profiles/minecraft/${username}`);
-                req.send();
+            } else if (type === 'online') {
+                axios.get(`https://api.mojang.com/users/profiles/minecraft/${username}`)
+                .then((response) => {
+                    if (response.status === 200) {
+                        console.log(response.data.id);
+                        user_info.id = response.data.id;
+                    }
+                });
             }
-            await interaction.reply({
-                content: `Created new entry for ${username}\n${JSON.stringify(user_info)}`,
-                ephemeral: true,
-            });
         } catch (err) {
-            console.log(err);
+            console.log(err)
+        } finally {
+            if (user_info.id != '')
+                await interaction.reply({
+                    content: `Created new entry for \`${username}\`\n\`${JSON.stringify(user_info)}\``,
+                    ephemeral: true,
+                });
+            else
+                await interaction.reply({
+                    content: `User \`${username}\` not found`,
+                    ephemeral: true,
+                });
         }
     }
 });
