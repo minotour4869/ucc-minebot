6212a2f91520eab8de28cc1f5fc8f114daa45e13
Minotour <quoclan9@gmail.com>
2023-12-21 21:52:51 +0700

    Implement adding entries for whitelist.json

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { Client, GatewayIntentBits } from "discord.js";
 import "dotenv/config"
 import uuidOffline from "./uuid.js";
 import axios from "axios";
+import fs from "fs"
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -15,35 +16,46 @@ client.on('interactionCreate', async interaction => {
     if (interaction.commandName == "link") {
         const username = interaction.options.get('username').value;
         const type = interaction.options.get('type').value;
+        const updateWhitelist = async (data) => {
+            data.id = data.id.substring(0, 8) + '-' +
+                      data.id.substring(8, 12) + '-' + 
+                      data.id.substring(12, 16) + '-' + 
+                      data.id.substring(16, 20) + '-' + 
+                      data.id.substring(20);
+        
+            let whitelist = JSON.parse(fs.readFileSync("whitelist.json"));
+            whitelist.push(data);
+            fs.writeFileSync("whitelist.json", JSON.stringify(whitelist));
+            await interaction.reply({
+                content: `Created new whitelist entry for \`${username}\``,
+                ephemeral: true,
+            });
+        }
         var user_info = {
+            id: null,
             name: username,
-            id: ''
         };
         try {
             if (type === 'offline') {
                 user_info.id = uuidOffline(username);
+                updateWhitelist(user_info);
             } else if (type === 'online') {
                 axios.get(`https://api.mojang.com/users/profiles/minecraft/${username}`)
                 .then((response) => {
-                    if (response.status === 200) {
-                        console.log(response.data.id);
-                        user_info.id = response.data.id;
-                    }
+                    if (response.status === 200) updateWhitelist(response.data);
+                    else throw "not_found";
+                })
+                .catch((err) => {
+                    if (err === "not_found") throw "not_found";
                 });
             }
         } catch (err) {
-            console.log(err)
-        } finally {
-            if (user_info.id != '')
-                await interaction.reply({
-                    content: `Created new entry for \`${username}\`\n\`${JSON.stringify(user_info)}\``,
-                    ephemeral: true,
-                });
-            else
+            if (err === "not_found")
                 await interaction.reply({
                     content: `User \`${username}\` not found`,
                     ephemeral: true,
                 });
+            console.log(err);
         }
     }
 });
--- src/uuid.js
+++ src/uuid.js
@@ -1,14 +1,18 @@
 import { md5 } from "js-md5"
 
 export default function uuidOffline(username) {
-    const str = "OfflinePlayer:" + username;
+    let str = "OfflinePlayer:" + username;
 
-    const textEncoder = new TextEncoder();
-
-    String.prototype.replaceAt = function (index, replacement) {
-        return this.substring(0, index) + replacement + this.substring(index + replacement.length);
-    }
-
-    let hash = md5(textEncoder.encode(str))
-    return hash.replaceAt(6, (hash[6] & 0x0f | 0x30)).replaceAt(8, (hash[8] & 0x3f | 0x80));
+    let hash = md5(str);
+    let byte_arr = []
+    for (let i = 0; i < hash.length; i += 2)
+        byte_arr.push(parseInt(hash.substring(i, i + 2), 16));
+    byte_arr[6] = byte_arr[6] & 0x0f | 0x30;
+    byte_arr[8] = byte_arr[8] & 0x3f | 0x80;
+    str = "";
+    byte_arr.forEach((byte) => {
+        str += ((byte >>> 4).toString(16));
+        str += ((byte & 0xF).toString(16));
+    });
+    return str;
 };
